3e49054a043591708709bf8e78f21d766db3641a
eunjilee0311 <leg040311@gmail.com>
2025-11-06 12:06:16 +0900

    Feat: 위치 좌표 고정된 지도 띄우기

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import KakaoMap from "./components/features/map";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>카카오맵 테스트</h2>
+        <KakaoMap />
+
+        <KakaoMap
+          width="80vw"
+          height="50vh"
+          center={{ lat: 35.1796, lng: 129.0756 }}
+          markerPosition={{ lat: 35.1796, lng: 129.0756 }}
+          markerLabel="부산"
+          level={4}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
