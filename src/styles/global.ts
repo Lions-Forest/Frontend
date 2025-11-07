bc85867893ad6dd4aff51ce6f18918e48c1c91a2
Lee <dlgudrud116@gmail.com>
2025-11-07 16:18:20 +0900

    Feat: 랜딩 페이지 UI 완성

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import dongleBold from '../assets/fonts/Dongle-Bold.ttf';
+import dongleRegular from '../assets/fonts/Dongle-Regular.ttf';
+import dongleLight from '../assets/fonts/Dongle-Light.ttf';
 
 // 전역 스타일 정의
 export const GlobalStyle = createGlobalStyle`
+  ${reset}
   * {
     margin: 0;
     padding: 0;
@@ -19,6 +24,22 @@ export const GlobalStyle = createGlobalStyle`
   #root {
     width: 100%;
     min-height: 100vh;
+  }
+  
+  /* font */
+  @font-face {
+    font-family: "dongleBold";
+    src: url(${dongleBold}) format('truetype');
+  }
+
+  @font-face {
+    font-family: "dongleRegular";
+    src: url(${dongleRegular}) format('truetype');
+  }
+
+  @font-face {
+    font-family: "dongleLight";
+    src: url(${dongleLight}) format('truetype');
   }
 `;
 
