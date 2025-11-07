d743bc466eb87ea08408f4c1086332d09fa3239a
eunjilee0311 <leg040311@gmail.com>
2025-11-08 00:07:14 +0900

    Feat: 로딩페이지 완성

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -5,13 +5,31 @@ import SpinnerIcon from "@/assets/images/LoadingLion.svg";
 export default function LoadingPage() {
   return (
     <PageWrap>
-      <Spinner>
-        <img src={SpinnerIcon} width={102} height={90} />
-      </Spinner>
-      <Description>
-        <Text>위치 정보를 불러오는 중</Text>
-        <Dots />
-      </Description>
+      <Container>
+        <Spinner>
+          <img src={SpinnerIcon} width={102} height={90} />
+        </Spinner>
+        <TextContainer>
+          <TextShadow>
+            <span>위치</span>
+            <span style={{ display: "inline-block", width: "5px" }} />
+            <span>로딩중</span>
+          </TextShadow>
+
+          <Text>
+            <ColoredText color="#FBBC04">위치</ColoredText>
+            <span style={{ width: "5px" }} />
+            <ColoredText color="#FF2370">로</ColoredText>
+            <ColoredText color="#0095FF">딩</ColoredText>
+            <ColoredText color="#43D687">중</ColoredText>
+
+            <Dots>
+              <DotShadow />
+              <DotText />
+            </Dots>
+          </Text>
+        </TextContainer>
+      </Container>
     </PageWrap>
   );
 }
@@ -21,65 +39,93 @@ const rotate = keyframes`
   to { transform: rotate(360deg); }
 `;
 
+const dotsAnimation = keyframes`
+    0%, 20% { content: ""; }
+    40% { content: "."; }
+    60% { content: ".."; }
+    80%, 100% { content: "..."; }
+`;
+
 const PageWrap = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 100vw;
-  height: 100vh;
+  max-width: 402px;
+  max-height: 874px;
+  width: 100%;
+  height: 100%;
   background-image: url(${Background});
   background-size: cover;
   background-position: center;
+`;
+
+const Container = styled.div`
+  gap: 27px;
 `;
 
 const Spinner = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-bottom: 27px;
 
   img {
-    animation: ${rotate} 1.5s linear infinite;
+    animation: ${rotate} 3.5s linear infinite;
   }
 `;
 
-const Description = styled.div`
-  width: 121px;
-  height: 80px;
+const TextContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: flex-end;
+  font-family: dongleBold;
   font-size: 64px;
   font-weight: 700;
-  border: #ffffff;
   text-shadow: 0 0 10px rgba(255, 255, 255, 0.7);
 `;
 
-const Text = styled.p``;
+const TextShadow = styled.div`
+  color: transparent;
+  -webkit-text-stroke: 6px #ffffff;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Text = styled.div`
+  position: relative;
+  display: flex;
+  gap: 0;
+`;
+
+const ColoredText = styled.span<{ color: string }>`
+  color: ${({ color }) => color};
+`;
 
 const Dots = styled.span`
   display: inline-block;
-  animation: dots 1.5s steps(3, end) infinite;
-  font-size: inherit;
+  position: relative;
+`;
+
+const DotShadow = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  color: transparent;
+  -webkit-text-stroke: 6px #ffffff;
+
+  &::after {
+    content: "";
+    animation: ${dotsAnimation} 2.5s steps(3, end) infinite;
+  }
+`;
+
+const DotText = styled.span`
+  position: relative;
   color: #43d687;
 
   &::after {
     content: "";
-    animation: dots 1.5s steps(4, end) infinite;
-  }
-
-  @keyframes dots {
-    0%,
-    20% {
-      content: "";
-    }
-    40% {
-      content: ".";
-    }
-    60% {
-      content: "..";
-    }
-    80%,
-    100% {
-      content: "...";
-    }
+    animation: ${dotsAnimation} 2.5s steps(3, end) infinite;
   }
 `;
