6c2df7c43b233e39b4da8c1098f920996e09f8da
eunjilee0311 <leg040311@gmail.com>
2025-11-08 22:32:24 +0900

    Feat: 커스텀 마커로 변경

--- src/components/features/map/BaseMap.tsx
+++ src/components/features/map/BaseMap.tsx
@@ -3,26 +3,32 @@ import { useAllLocations } from "@/hooks/useAllLocations";
 import { useMyLocation } from "@/hooks/useMyLocation";
 import { useEffect, useState } from "react";
 import LoadingPage from "@/pages/LoadingPage";
+import MarkerImage from "@/assets/images/LoadingLion.svg"; // 임시로 다른 사진 지정
 
 export default function BaseMap({ userId }: { userId: string }) {
-  // useMyLocation(userId); // 내 위치 Firebase에 업로드
-  // const locations = useAllLocations(); // 모든 사용자 위치 구독
-  // const myLocation = locations?.[userId]; // 모든 사용자 위치 중 내 위치 찾아내기 (중심 좌표를 찾기 위해)
-  // const [center, setCenter] = useState<{ lat: Number; lng: number } | null>(
-  //   null
-  // );
+  useMyLocation(userId); // 내 위치 Firebase에 업로드
+  const locations = useAllLocations(); // 모든 사용자 위치 구독
+  const myLocation = locations?.[userId]; // 모든 사용자 위치 중 내 위치 찾아내기 (중심 좌표를 찾기 위해)
+  const [center, setCenter] = useState<{ lat: Number; lng: number } | null>(
+    null
+  );
+  const markerImage = {
+    src: MarkerImage,
+    size: { width: 50, height: 50},
+    options: { offset: { x: 25, y: 50 }}, // 마커 중심 조정
+  };
 
-  // useEffect(() => {
-  //   if (myLocation) {
-  //     setCenter({ lat: myLocation.latitude, lng: myLocation.longitude });
-  //   }
-  // }, [myLocation]);
+  useEffect(() => {
+    if (myLocation) {
+      setCenter({ lat: myLocation.latitude, lng: myLocation.longitude });
+    }
+  }, [myLocation]);
 
-  // // 아직 위치 데이터가 들어오지 않았다면 로딩 화면 표시
-  // const isLoading = !locations || !myLocation;
-  // if (isLoading) return <LoadingPage />;
+  // 아직 위치 데이터가 들어오지 않았다면 로딩 화면 표시
+  const isLoading = !locations || !myLocation;
+  if (isLoading) return <LoadingPage />;
 
-  const myLocation = { latitude: 37.5665, longitude: 126.978 };
+  // const myLocation = { latitude: 37.5665, longitude: 126.978 };
 
   return (
     <Map
@@ -30,7 +36,7 @@ export default function BaseMap({ userId }: { userId: string }) {
       style={{ width: "100%", height: "100%" }}
       level={4}
     >
-      {/* 모든 사용자 위치에 마커 표시 -> locatioins의 각 키(userId)와 값(loc)을 순회
+      모든 사용자 위치에 마커 표시 -> locatioins의 각 키(userId)와 값(loc)을 순회
       {Object.entries(locations).map(([id, loc]) =>
         loc?.latitude && loc?.longitude ? (
           <MapMarker
@@ -39,9 +45,10 @@ export default function BaseMap({ userId }: { userId: string }) {
             title={id === userId ? "내 위치" : `사용자 ${id}`} // 마커에 마우스 올렸을 때 보일 텍스트
           />
         ) : null
-      )} */}
+      )}
       <MapMarker
         position={{ lat: myLocation.latitude, lng: myLocation.longitude }}
+        image={markerImage}
         title="내 위치"
       />
     </Map>
