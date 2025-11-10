fb9110dd2cdb6bbc11391c5ae6fafda93d025ae4
gun587330 <gun5873@naver.com>
2025-11-10 23:56:49 +0900

    Refactor: MyPage에 MyInfoCard 컴포넌트 적용

--- src/pages/Mypage/index.tsx
+++ src/pages/Mypage/index.tsx
@@ -1,11 +1,28 @@
+import MyActivities from "@/components/features/MyActivities";
+import MyInfoCard from "@/components/features/MyInfoCard";
 import Layout from "@/components/layout/Layout";
+import styled from "styled-components";
 
 function index() {
     return(
         <Layout>
-            hi
+            <MyPageLayout>
+                <MyInfoCard />
+            </MyPageLayout>
         </Layout>
     )
 }
 
 export default index;
+
+const MyPageLayout = styled.div`
+    background: #E4F2EA;
+    width: 100%;
+    height: auto;
+    // overflow: auto;
+    display: flex;
+    padding: 16px 0px 14px 0px;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+`;
