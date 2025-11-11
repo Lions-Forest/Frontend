da5c03bb6415e50d19d18b40f5ce7fbc1f3e1f9d
gun587330 <gun5873@naver.com>
2025-11-11 20:42:28 +0900

    Refactor: NotificationPage(알림 페이지)에 NotificationCard 컴포넌트 적용(임시)

--- src/pages/NotificationPage/index.tsx
+++ src/pages/NotificationPage/index.tsx
@@ -1,8 +1,106 @@
+import BackToNavBar from "@/components/common/BackToNavBar";
+import styled from 'styled-components';
+import NotificationCard from '@/components/features/NotificationCard';
+
+// NotificationCard props interface 정의(dummy data로 임시)
+interface NotificationCardProps {
+  meetingDate: string;
+  meetingName: string;
+  imageUrl?: string;
+}
+
 function index() {
+    // Dummy data (임시)
+    const dummyNotifications: NotificationCardProps[] = [
+        {
+            meetingDate: "25.11.07",
+            meetingName: "카공 번개",
+            imageUrl: "https://i.ifh.cc/9p1o6z.jpg"
+        },
+        {
+            meetingDate: "25.11.06",
+            meetingName: "스터디 그룹",
+            imageUrl: "https://i.ifh.cc/9p1o6z.jpg"
+        },
+        {
+            meetingDate: "25.11.05",
+            meetingName: "점심 식사",
+            // 이미지 Url 없이
+        },
+        {
+            meetingDate: "25.11.06",
+            meetingName: "스터디 그룹",
+            imageUrl: "https://i.ifh.cc/9p1o6z.jpg"
+        },
+        {
+            meetingDate: "25.11.06",
+            meetingName: "스터디 그룹",
+            imageUrl: "https://i.ifh.cc/9p1o6z.jpg"
+        },
+        {
+            meetingDate: "25.11.06",
+            meetingName: "스터디 그룹",
+            imageUrl: "https://i.ifh.cc/9p1o6z.jpg"
+        },
+        {
+            meetingDate: "25.11.06",
+            meetingName: "스터디 그룹",
+            imageUrl: "https://i.ifh.cc/9p1o6z.jpg"
+        },
+        {
+            meetingDate: "25.11.06",
+            meetingName: "스터디 그룹",
+            imageUrl: "https://i.ifh.cc/9p1o6z.jpg"
+        },
+        {
+            meetingDate: "25.11.06",
+            meetingName: "스터디 그룹",
+            imageUrl: "https://i.ifh.cc/9p1o6z.jpg"
+        },
+        {
+            meetingDate: "25.11.06",
+            meetingName: "스터디 그룹",
+            imageUrl: "https://i.ifh.cc/9p1o6z.jpg"
+        },
+        {
+            meetingDate: "25.11.06",
+            meetingName: "스터디 그룹",
+            imageUrl: "https://i.ifh.cc/9p1o6z.jpg"
+        },
+        {
+            meetingDate: "25.11.06",
+            meetingName: "스터디 그룹",
+            imageUrl: "https://i.ifh.cc/9p1o6z.jpg"
+        },
+
+    ];
+
     return(
-        <>
-        hello
-        </>
+        <Wrapper>
+            <BackToNavBar text="알림" />
+            <Contents>
+                {dummyNotifications.map((notification, index) => (
+                    <NotificationCard key={index} {...notification} />
+                ))}
+            </Contents>
+        </Wrapper>
     )
 }
 export default index;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+//  overflow: hidden;
+`;
+
+const Contents = styled.div`
+  /* font-size: 100px; */ /* Removed placeholder styling */
+
+  flex: 1;
+  width: 100%;
+  overflow-y: auto;
+  overflow-x: hidden;
+`;
