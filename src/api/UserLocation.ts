45e49168e6d3eafa659614e7958641de4e1e9055
gun587330 <75376944+gun587330@users.noreply.github.com>
2025-11-12 19:28:16 +0900

    Revert "Feature mypage 반영 + API (중앙 baseURL관리 apis/client.ts, 로그인 통신 apis/user/login.ts)"

--- src/api/UserLocation.ts
+++
@@ -1,8 +0,0 @@
-export interface UserLocation {
-  userId: string;
-  name: string;
-  latitude: number;
-  longitude: number;
-  status: "studying" | "eating" | "offline";
-  shareLocation: boolean;
-}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,12 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
-import reset from 'styled-reset';
-import dongleBold from '../assets/fonts/Dongle-Bold.ttf';
-import dongleRegular from '../assets/fonts/Dongle-Regular.ttf';
-import dongleLight from '../assets/fonts/Dongle-Light.ttf';
 
 // 전역 스타일 정의
 export const GlobalStyle = createGlobalStyle`
-  ${reset}
   * {
     margin: 0;
     padding: 0;
@@ -25,84 +20,5 @@ export const GlobalStyle = createGlobalStyle`
     width: 100%;
     min-height: 100vh;
   }
-  
-  /* font */
-  @font-face {
-    font-family: "dongleBold";
-    src: url(${dongleBold}) format('truetype');
-  }
-
-  @font-face {
-    font-family: "dongleRegular";
-    src: url(${dongleRegular}) format('truetype');
-  }
-
-  @font-face {
-    font-family: "dongleLight";
-    src: url(${dongleLight}) format('truetype');
-  }
-
-  @font-face {
-    font-family: 'Pretendard';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/pretendard@1.0/Pretendard-Thin.woff2') format('woff2');
-    font-weight: 100;
-    font-display: swap;
-}
-
-@font-face {
-    font-family: 'Pretendard';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/pretendard@1.0/Pretendard-ExtraLight.woff2') format('woff2');
-    font-weight: 200;
-    font-display: swap;
-}
-
-@font-face {
-    font-family: 'Pretendard';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/pretendard@1.0/Pretendard-Light.woff2') format('woff2');
-    font-weight: 300;
-    font-display: swap;
-}
-
-@font-face {
-    font-family: 'Pretendard';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/pretendard@1.0/Pretendard-Regular.woff2') format('woff2');
-    font-weight: 400;
-    font-display: swap;
-}
-
-@font-face {
-    font-family: 'Pretendard';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/pretendard@1.0/Pretendard-Medium.woff2') format('woff2');
-    font-weight: 500;
-    font-display: swap;
-}
-
-@font-face {
-    font-family: 'Pretendard';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/pretendard@1.0/Pretendard-SemiBold.woff2') format('woff2');
-    font-weight: 600;
-    font-display: swap;
-}
-
-@font-face {
-    font-family: 'Pretendard';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/pretendard@1.0/Pretendard-Bold.woff2') format('woff2');
-    font-weight: 700;
-    font-display: swap;
-}
-
-@font-face {
-    font-family: 'Pretendard';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/pretendard@1.0/Pretendard-ExtraBold.woff2') format('woff2');
-    font-weight: 800;
-    font-display: swap;
-}
-
-@font-face {
-    font-family: 'Pretendard';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/pretendard@1.0/Pretendard-Black.woff2') format('woff2');
-    font-weight: 900;
-    font-display: swap;
-}
 `;
 
