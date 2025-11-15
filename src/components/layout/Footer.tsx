8ea2124690d5155c614cb0689ebbce52aac2cd6b
gun587330 <gun5873@naver.com>
2025-11-15 23:49:38 +0900

    Refactor: Footer에 홈 라우팅 이벤트 추가

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,27 +5,35 @@ import { MdDiversity3 as Home } from "react-icons/md";
 import { MdAccountCircle as My } from "react-icons/md";
 
 function Footer() {
-  // 마이페이지 라우팅을 위한 네비게이션 훅 정의_p.s. 정건(시작) //
+  // 홈/마이페이지 라우팅을 위한 네비게이션 훅 정의_p.s. 정건(시작) //
   const navigate = useNavigate();
   const location = useLocation();
   
+  // 현재 경로가 홈인지 확인
+  const isHomeActive = location.pathname.startsWith('/home');
+  
   // 현재 경로가 마이페이지인지 확인
   const isMyPageActive = location.pathname.startsWith('/mypage');
+
+  // 홈으로 이동하는 핸들러
+  const handleHomeClick = () => {
+    navigate('/home');
+  };
 
   // 마이페이지로 이동하는 핸들러
   const handleMyPageClick = () => {
     navigate('/mypage');
   };
-  //// 마이페이지 라우팅을 위한 네비게이션 훅 정의_p.s. 정건(끝) //
+  //// 홈/마이페이지 라우팅을 위한 네비게이션 훅 정의_p.s. 정건(끝) //
   return (
     <FooterLayout>
       <Section>
         <MapIcon />
         <Title>지도</Title>
       </Section>
-      <Section>
-        <HomeIcon />
-        <Title>홈</Title>
+      <Section onClick={handleHomeClick}> {/*핸들러 추가 p.s. 정건(시작)*/}
+        <HomeIcon $active={isHomeActive} /> {/* 브라우저로 판단 후 varient 적용 정건*/}
+        <Title $active={isHomeActive}>홈</Title> {/*핸들러 추가 p.s. 정건(끝)*/}
       </Section>
       <Section onClick={handleMyPageClick}> {/*핸들러 추가 p.s. 정건(시작)*/}
         <MyIcon $active={isMyPageActive} /> {/* 브라우저로 판단 후 varient 적용 정건*/}
@@ -69,13 +77,11 @@ const MapIcon = styled(Map)`
     color}; // TODO: 페이지 따라서 onClick 기능 + 색 변화 구현
 `;
 
-const HomeIcon = styled(Home)`
+const HomeIcon = styled(Home)<{ $active?: boolean }>` // 브라우저로 판단 후 varient 적용_p.s. 정건
   width: 24px;
   height: 24px;
-  fill: #1c1b1f66;
-  opacity: 40%;
-  // fill: ${({ color }) =>
-    color}; // TODO: 페이지 따라서 onClick 기능 + 색 변화 구현
+  fill: ${({ $active }) => ($active ? '#43D687' : '#1c1b1f66')}; // 브라우저로 판단 후 varient 적용_p.s. 정건
+  opacity: ${({ $active }) => ($active ? '100%' : '40%')}; // 브라우저로 판단 후 varient 적용_p.s. 정건
 `;
 
 const MyIcon = styled(My)<{ $active?: boolean }>` // 브라우저로 판단 후 varient 적용_p.s. 정건
