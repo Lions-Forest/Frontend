1d9d652578181cdf0f9322c9f3e9bdc27d5ec6c9
eunjilee0311 <leg040311@gmail.com>
2025-11-15 21:40:11 +0900

    Refactor: 인증 상태 분리

--- src/pages/Map/MapPage.tsx
+++ src/pages/Map/MapPage.tsx
@@ -1,8 +1,13 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import BaseMap from "./BaseMap";
 import { useEffect, useState } from "react";
+import LoadingPage from "./LoadingPage";
+import { Navigate } from "react-router-dom";
 
 export default function MapPage() {
+  const [authStatus, setAuthStatus] = useState<
+    "loading" | "authed" | "unauthed"
+  >("loading");
   const [userId, setUserId] = useState<string | null>(null);
   const [name, setName] = useState<string>("");
 
@@ -12,14 +17,22 @@ export default function MapPage() {
       if (user) {
         setUserId(user.uid);
         setName(user.displayName || user.email?.split("@")[0] || "사용자");
+        setAuthStatus("authed");
       } else {
         setUserId(null);
+        setAuthStatus("unauthed");
       }
     });
     return () => unsubscribe();
   }, []);
 
-  if (!userId) return <>로그인이 필요합니다.</>;
+  if (authStatus === "loading") {
+    return <LoadingPage />;
+  }
 
-  return <BaseMap userId={userId} name={name} />;
+  if (authStatus === "unauthed") {
+    return <Navigate to="/" replace />;
+  }
+
+  return <BaseMap userId={userId!} name={name} />;
 }
