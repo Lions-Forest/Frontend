6a53fdddcf2e445ce77975655fde49b1543ee279
gun587330 <gun5873@naver.com>
2025-11-16 13:53:57 +0900

    Refactor: PWA (vite_plugin) 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { registerSW } from "virtual:pwa-register";
 
 const queryClient = new QueryClient();
+
+registerSW();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["src/assets/lions-forest.ico"],
+      manifest: {
+        name: "모여봐요 사자의숲",
+        short_name: "사자의숲",
+        description: "모여봐요 사자의숲 PWA",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#E4F2EA",
+        theme_color: "#E4F2EA",
+        icons: [
+          {
+            src: "/lions-forest.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
