59a08cf212d36f24212b7286f28ec427c30ecc86
gun587330 <gun5873@naver.com>
2025-11-16 19:06:25 +0900

    Refactor: pwa favicon임시 추가

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["src/assets/lions-forest.ico"],
+      includeAssets: ["src/assets/lions-forest.ico", "src/assets/icon/favicon.png"],
       manifest: {
         name: "모여봐요 사자의숲",
         short_name: "사자의숲",
@@ -21,9 +21,16 @@ export default defineConfig({
         theme_color: "#E4F2EA",
         icons: [
           {
-            src: "/lions-forest.ico",
-            sizes: "64x64 32x32 24x24 16x16",
-            type: "image/x-icon",
+            src: "/assets/icon/favicon.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/assets/icon/favicon.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
           },
         ],
       },
