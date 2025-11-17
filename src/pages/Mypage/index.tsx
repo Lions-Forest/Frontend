c70e4c552c91775be154af9e78ba4d61575f6731
gun587330 <gun5873@naver.com>
2025-11-18 01:39:57 +0900

    Design: mypage 배경 y값깨지는 현상 보수

--- src/pages/Mypage/index.tsx
+++ src/pages/Mypage/index.tsx
@@ -20,7 +20,7 @@ export default index;
 const MyPageLayout = styled.div`
     background: #E4F2EA;
     width: 100%;
-    height: auto;
+    height: 100%;
     // overflow: auto;
     display: flex;
     padding: 0px 0px 14px 0px;
